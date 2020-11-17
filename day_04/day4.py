9681170e4af3945af5d035b392e9e94d4aa3b3c8
Alexander Colen <alexanderc@live.nl>
2020-11-17 16:18:11 -0500

    Day 4 puzzle 1

---
+++ day_04/day4.py
@@ -0,0 +1,20 @@
+import hashlib
+
+
+def puzzle1(instructions: str):
+    current = 0
+    while True:
+        result = hashlib.md5(f'{instructions}{current}'.encode())
+        if result.hexdigest()[0:5] == '00000':
+            return current
+        current += 1
+
+
+def puzzle2():
+    return 0
+
+
+if __name__ == '__main__':
+    puzzle_input = 'bgvyzdsv'
+    print(f'Puzzle 1 solution: {puzzle1(instructions=puzzle_input)}')
+    print(f'Puzzle 2 solution: {puzzle2()}')
