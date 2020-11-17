dd684ab199ca9d7ac2f5e62e20b45dc3150ee279
Alexander Colen <alexanderc@live.nl>
2020-11-17 17:47:39 -0500

    Day 5 puzzle 2

--- day_05/day5.py
+++ day_05/day5.py
@@ -1,3 +1,6 @@
+from itertools import combinations
+
+
 def parse_input():
     strings = []
     with open('input', 'r') as file:
@@ -24,11 +27,37 @@ def puzzle1(instructions: []):
     return nice
 
 
-def puzzle2():
-    return 0
+def puzzle2(instructions: []):
+    nice = 0
+    for s in instructions:
+        pair = False
+        triple = False
+        for num, c in enumerate(s):
+            # Check for pair.
+            if not pair:
+                if num + 1 < len(s):
+                    p = s[num:num+2]
+                    if p[0] != p[1]:
+                        pair = s.count(p) > 1
+                    else:
+                        try:
+                            if p[0] == s[num:num+3][2]:
+                                pair = s.count(p) > 2
+                            else:
+                                pair = s.count(p) > 1
+                        except IndexError:
+                            pair = s.count(p) > 1
+            # Check for triple.
+            if not triple:
+                if num + 2 < len(s):
+                    triple = c == s[num:num+3][2]
+            if pair and triple:
+                nice += 1
+                break
+    return nice
 
 
 if __name__ == '__main__':
     parsed_input = parse_input()
     print(f'Puzzle 1 solution: {puzzle1(instructions=parsed_input)}')
-    print(f'Puzzle 2 solution: {puzzle2()}')
+    print(f'Puzzle 2 solution: {puzzle2(instructions=parsed_input)}')
