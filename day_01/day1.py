97cbdd7108ffffd040c5a50de76d9c8c29bc0a8e
Alexander Colen <alexanderc@live.nl>
2020-11-17 15:32:49 -0500

    Day 1 puzzle 2

--- day_01/day1.py
+++ day_01/day1.py
@@ -10,11 +10,18 @@ def puzzle1(instructions: str):
     return 0 + up_count - down_count
 
 
-def puzzle2():
-    return 0
+def puzzle2(instructions: str):
+    floor = 0
+    for num, c in enumerate(instructions):
+        if c == '(':
+            floor += 1
+        else:
+            floor -= 1
+            if floor < 0:
+                return num + 1
 
 
 if __name__ == '__main__':
     parsed_instructions = parse_input()
     print(f'Puzzle 1 solution: {puzzle1(instructions=parsed_instructions)}')
-    print(f'Puzzle 2 solution: {puzzle2()}')
+    print(f'Puzzle 2 solution: {puzzle2(instructions=parsed_instructions)}')
