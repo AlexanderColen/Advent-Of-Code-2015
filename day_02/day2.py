4d9620628ee2c6f2a7615f9210d32755c3197394
Alexander Colen <alexanderc@live.nl>
2020-11-17 15:46:35 -0500

    Day 2 puzzle 2

--- day_02/day2.py
+++ day_02/day2.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def parse_input():
     dimensions = []
     with open('input', 'r') as file:
@@ -17,11 +20,20 @@ def puzzle1(instructions: []):
     return wrapping_paper
 
 
-def puzzle2():
-    return 0
+def puzzle2(instructions: []):
+    total_ribbon = 0
+    for instruction in instructions:
+        instruction = [int(x) for x in instruction]
+        # Ribbon: Shortest distance around its sides, or the smallest perimeter of any one face.
+        A, B = np.partition(instruction, 1)[0:2]
+        ribbon = 2*A + 2*B
+        # Bow: Cubic feet of volume of the present
+        bow = instruction[0] * instruction[1] * instruction[2]
+        total_ribbon += ribbon + bow
+    return total_ribbon
 
 
 if __name__ == '__main__':
     parsed_input = parse_input()
     print(f'Puzzle 1 solution: {puzzle1(instructions=parsed_input)}')
-    print(f'Puzzle 2 solution: {puzzle2()}')
+    print(f'Puzzle 2 solution: {puzzle2(instructions=parsed_input)}')
